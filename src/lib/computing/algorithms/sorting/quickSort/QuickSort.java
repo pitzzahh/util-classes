48b02af4bc31fb08e19c0e1ceb98a0012c1f3653
pitzzahh <97332554+pitzzahh@users.noreply.github.com>
2022-02-09 14:30:44 +0800

    added double array sorting

--- src/lib/computing/algorithms/sorting/quickSort/QuickSort.java
+++ src/lib/computing/algorithms/sorting/quickSort/QuickSort.java
@@ -53,6 +53,20 @@ public final class QuickSort {
         printArray(array);
     }
     /**
+     * Sort the double array
+     * @param array double array that will be sorted
+     */
+    public static void sort(double[] array) {
+        System.out.println("********************************************");
+        System.out.println("* QUICK SORT ALGORITHM THAT SORTS AN ARRAY *");
+        System.out.println("********************************************");
+        System.out.print("BEFORE SORTING: ");
+        printArray(array);
+        quickSort(array, 0, array.length - 1);
+        System.out.print("\nAFTER SORTING : ");
+        printArray(array);
+    }
+    /**
      * The Algorithm that sort the byte array.
      * @param arrayToSort the array to sort.
      * @param lowIndex the arrayToSort at index 0
@@ -98,6 +112,23 @@ public final class QuickSort {
         }
         int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
         int pivot = arrayToSort[pivotIndex];
+        swap(arrayToSort, pivotIndex, highIndex);
+        int leftPointer = partition(arrayToSort, lowIndex, highIndex, pivot);
+        quickSort(arrayToSort, lowIndex, leftPointer - 1);
+        quickSort(arrayToSort, leftPointer + 1, highIndex);
+    }
+    /**
+     * The Algorithm that sort the int array
+     * @param arrayToSort the array to sort
+     * @param lowIndex the arrayToSort at index 0
+     * @param highIndex the last index of the arrayToSort int array
+     */
+    private static void quickSort(double[] arrayToSort, int lowIndex, int highIndex) {
+        if (lowIndex >= highIndex) {
+            return;
+        }
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        double pivot = arrayToSort[pivotIndex];
         swap(arrayToSort, pivotIndex, highIndex);
         int leftPointer = partition(arrayToSort, lowIndex, highIndex, pivot);
         quickSort(arrayToSort, lowIndex, leftPointer - 1);
@@ -170,6 +201,28 @@ public final class QuickSort {
         return leftPointer;
     }
     /**
+     * The Algorithm that partitions the double array
+     * @param arrayToSort the array to sort
+     * @param lowIndex the arrayToSort at index 0
+     * @param highIndex the last index of the arrayToSort array
+     * @param pivot the position that is selected in the array that will be compared to the sub arrays
+     */
+    private static int partition(double[] arrayToSort, int lowIndex, int highIndex, double pivot) {
+        int rightPointer = highIndex;
+        int leftPointer = lowIndex;
+        while (leftPointer < rightPointer) {
+            while (arrayToSort[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+            while (arrayToSort[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+            swap(arrayToSort, leftPointer, rightPointer);
+        }
+        swap(arrayToSort, leftPointer, highIndex);
+        return leftPointer;
+    }
+    /**
      * Method that swaps the byte array
      * @param arrayToSwap the byte array to swap
      * @param index1 the first index
@@ -188,6 +241,17 @@ public final class QuickSort {
      */
     private static void swap(short[] arrayToSwap, int index1, int index2) {
         short temporary = arrayToSwap[index1];
+        arrayToSwap[index1] = arrayToSwap[index2];
+        arrayToSwap[index2] = temporary;
+    }
+    /**
+     * Method that swaps the double array
+     * @param arrayToSwap the double array to swap
+     * @param index1 the first index
+     * @param index2 the second index
+     */
+    private static void swap(double[] arrayToSwap, int index1, int index2) {
+        double temporary = arrayToSwap[index1];
         arrayToSwap[index1] = arrayToSwap[index2];
         arrayToSwap[index2] = temporary;
     }
@@ -231,6 +295,18 @@ public final class QuickSort {
      * @param arrayToPrint the int array to be printed
      */
     private static void printArray(int[] arrayToPrint) {
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.print(arrayToPrint[i]);
+            if (i != arrayToPrint.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+    /**
+     * Method that prints the double array
+     * @param arrayToPrint the double array to be printed
+     */
+    private static void printArray(double[] arrayToPrint) {
         for (int i = 0; i < arrayToPrint.length; i++) {
             System.out.print(arrayToPrint[i]);
             if (i != arrayToPrint.length - 1) {
