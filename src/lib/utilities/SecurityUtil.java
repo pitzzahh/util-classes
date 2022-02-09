a732e8210a695ea63630c47c59a2baee0321d2e5
pitzzahh <97332554+pitzzahh@users.noreply.github.com>
2022-02-09 21:18:59 +0800

    refactored

--- src/lib/utilities/SecurityUtil.java
+++ src/lib/utilities/SecurityUtil.java
@@ -24,36 +24,48 @@ public final class SecurityUtil {
     private SecurityUtil() {
     }
     /**
-     * Encrypts the user's username and password.
-     * @param usernameFile the file of where the username contains
-     * @param pinFile the file where the password contains
-     * @throws IOException if the file does not exist
+     * Encrypts the user's username.
+     * @param usernameFile the file of where the username contained.
+     * @throws IOException if the file does not exist.
      */
-    public static void encrypt(File usernameFile, File pinFile, String user) throws IOException {
-        if (usernameFile.isFile() && pinFile.isFile()) {
+    public static void encryptUserName(File usernameFile, String user) throws IOException {
+        if (usernameFile.isFile()) {
             uScanner = new Scanner(usernameFile);
-            pScanner = new Scanner(pinFile);
             u = uScanner.nextLine();
-            p = pScanner.nextLine();
             uChar = u.toCharArray();
-            pChar = p.toCharArray();
             uScanner.close();
-            pScanner.close();
-            for (char elements : uChar) {
-                elements += (KEY * (-10));
-                EU.add(elements);
-            }
-            for (char elements : pChar) {
-                elements += (KEY * (-10) + 4);
-                EP.add(elements);
+            for (char element : uChar) {
+                element += (KEY * (-10));
+                EU.add(element);
             }
             u = ArrayUtil.getCharacters(EU);
-            p = ArrayUtil.getCharacters(EP);
             FileUtil.writeToATextFile(u, usernameFile);
-            FileUtil.writeToATextFile(p, pinFile);
-            EU.clear();
             EP.clear();
-            writeToBin(usernameFile, pinFile, user);
+            writeUserNameToBin(usernameFile, user);
+        }
+        else {
+            System.out.println("SOMETHING WENT WRONG");
+        }
+    }
+    /**
+     * Encrypts the user's pin.
+     * @param pinFile the file where the pin contained.
+     * @throws IOException if the file does not exist.
+     */
+    public static void encryptPin(File pinFile, String user) throws IOException {
+        if (pinFile.isFile()) {
+            pScanner = new Scanner(pinFile);
+            p = pScanner.nextLine();
+            pChar = p.toCharArray();
+            pScanner.close();
+            for (char element : pChar) {
+                element += (KEY * (-10) + 4);
+                EP.add(element);
+            }
+            p = ArrayUtil.getCharacters(EP);
+            FileUtil.writeToATextFile(p, pinFile);
+            EP.clear();
+            writePinToBin(pinFile, user);
         }
         else {
             System.out.println("SOMETHING WENT WRONG");
@@ -61,8 +73,8 @@ public final class SecurityUtil {
     }
     /**
      * Checks if the username and password matched the user's credentials.
-     * @param usernameFile the file of where the username contains.
-     * @param pinFile the file where the password contains.
+     * @param usernameFile the file of where the username contained.
+     * @param pinFile the file where the password contained.
      * @throws IOException if the file does not exist.
      */
     public static boolean checkCredentials(File usernameFile, File pinFile, String username, String pin, boolean isAdmin) throws IOException {
@@ -93,23 +105,31 @@ public final class SecurityUtil {
 
     /**
      * Method that reWrite the user's username to a binary file.
-     * @param usernameFile the file of where the username contains.
-     * @param pinFile the file where the password contains.
+     * @param usernameFile the file of where the username contained.
      * @throws IOException if the file does not exist.
      */
-    private static void writeToBin(File usernameFile, File pinFile, String user) throws IOException {
+    private static void writeUserNameToBin(File usernameFile, String user) throws IOException {
         try {
             uScanner = new Scanner(usernameFile);
-            pScanner = new Scanner(pinFile);
             u = uScanner.nextLine();
-            p = pScanner.nextLine();
             DataOutputStream dataOutputStreamForUserName = new DataOutputStream(new FileOutputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\uCache.bin"));
-            DataOutputStream dataOutputStreamForPin = new DataOutputStream(new FileOutputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\pCache.bin"));
             dataOutputStreamForUserName.writeUTF(u);
-            dataOutputStreamForPin.writeUTF(p);
             dataOutputStreamForUserName.flush();
-            dataOutputStreamForPin.flush();
             dataOutputStreamForUserName.close();
+        } catch (FileNotFoundException ignored) {}
+    }
+    /**
+     * Method that reWrite the user's pin to a binary file.
+     * @param pinFile the file where the pin contained.
+     * @throws IOException if the file does not exist.
+     */
+    private static void writePinToBin(File pinFile, String user) throws IOException {
+        try {
+            pScanner = new Scanner(pinFile);
+            p = pScanner.nextLine();
+            DataOutputStream dataOutputStreamForPin = new DataOutputStream(new FileOutputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\pCache.bin"));
+            dataOutputStreamForPin.writeUTF(p);
+            dataOutputStreamForPin.flush();
             dataOutputStreamForPin.close();
         } catch (FileNotFoundException ignored) {}
     }
@@ -117,8 +137,8 @@ public final class SecurityUtil {
      * Method that rechecks the user's credentials.
      * <p>Even though the user copied and pasted the username and password from the txt file, it still won't logged in
      * unless the user really know his/her credentials.</p>
-     * @param usernameFile the file of where the username contains.
-     * @param pinFile the file where the password contains.
+     * @param usernameFile the file of where the username contained.
+     * @param pinFile the file where the password contained.
      * @throws IOException if the file does not exist.
      */
     private static boolean readFromBin(File usernameFile, File pinFile, String user, boolean isAdmin) throws IOException {
