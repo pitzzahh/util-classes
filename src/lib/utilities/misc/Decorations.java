bf82cfaef73df6138e7fe4398866a1dcefd0ad44
pitzzahh <97332554+pitzzahh@users.noreply.github.com>
2022-02-12 23:59:45 +0800

    added text color options

--- src/lib/utilities/misc/Decorations.java
+++ src/lib/utilities/misc/Decorations.java
@@ -1,22 +1,32 @@
 package lib.utilities.misc;
 
-import lib.systems.studentManagementSystem.processes.framework.Process;
-import lib.systems.studentManagementSystem.main.MainActivity;
-
-public final class Decorations extends Process {
+public final class Decorations {
     /**
-     * Cannot instantiate this class
+     * Cannot instantiate this class.
      */
-    private Decorations() {
-        super();
-    }
+    private Decorations() {}
 
+    public static final Prompt show = new Prompt();
+
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_BLACK = "\u001B[30m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String TEXT_PURPLE = "\u001B[35m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String TEXT_WHITE = "\u001B[37m";
     /**
      * Inserts a line above and below descriptions.
-     * <p> Called in: {@link MainActivity#run() run()}, {@link Process#createStudent() createStudent()},
-     * {@link Process#tryAgain() tryAgain()}, {@link Process#showList() showList()}, {@link Process#sortStudentList() sortStudentList()} methods
      */
     public static void line() {
         System.out.println(":---------------------------------------------:");
+    }
+    /**
+     * Prints a loading text in color green.
+     */
+    public static void printLoading() {
+        System.out.print(Decorations.TEXT_GREEN + "LOADING" + Decorations.TEXT_RESET);
     }
 }
