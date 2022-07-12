935dd153266ecfdde0e7b55aabb41afa0862f24d
pitzzahh <araopj2191@gmail.com>
2022-07-12 20:26:14 +0800

    added the possible operations, and refactored the abstract method.

--- src/main/java/com/github/pitzzahh/computing/Operation.java
+++ src/main/java/com/github/pitzzahh/computing/Operation.java
@@ -3,6 +3,11 @@ package com.github.pitzzahh.computing;
 @FunctionalInterface
 public interface Operation {
 
-    Number op(Number a, Number b, int op);
+    Number calculate(int operation) throws IllegalArgumentException;
 
+     int MULTIPLY = 1;
+     int DIVIDE = 2;
+     int ADD = 3;
+     int SUBTRACT = 4;
+     int MODULO = 5;
 }
