796d2563cab25e82248a1b76eb24a6673e695f94
peter <araopj2191@gmail.com>
2022-07-15 23:58:39 +0800

    refactored Collatz class.

--- src/main/java/com/github/pitzzahh/computing/Collatz.java
+++ src/main/java/com/github/pitzzahh/computing/Collatz.java
@@ -1,45 +1,47 @@
 package com.github.pitzzahh.computing;
 
-import java.util.Scanner;
+import lombok.NoArgsConstructor;
+import java.util.stream.IntStream;
 import java.util.concurrent.TimeUnit;
 import com.github.pitzzahh.utilities.validation.Validator;
 
-// TODO: refactor to generics
+@NoArgsConstructor
 public final class Collatz {
-    // Cannot instantiate this class
-    private Collatz() {}
 
-    public static void runCollatzConjecture() throws InterruptedException {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String userInput;
-            int number = 0;
-            boolean inputChecker = true;
-            while (inputChecker) {
-                System.out.print("Enter a number: ");
-                userInput = scanner.next().trim();
-                if (Validator.isWholeNumber().apply(userInput)) {
-                    number = Integer.parseInt(userInput);
-                    if (number >= 1)
-                        inputChecker = false;
-                    else
-                        System.out.println("Enter a number that is greater than or equal to 1");
-                }
-                else
-                    System.out.println("PLEASE ENTER A NUMBER");
+    private static int number;
+
+    /**
+     * Outputs a String
+     * @param withDelay {@code true} a delay will be added on printing the output.
+     */
+    public void runCollatzConjecture(boolean withDelay) {
+        IntStream.range(1, number).forEach(i -> {
+            if (number % 2 != 0) {
+                number = (number * 3) + 1;
+                System.out.printf("Number %d : %d is ODD\n", i, number);
             }
-            for (int i = 1; number != 1; i++) {
-                if (number % 2 != 0) {
-                    number = (number * 3) + 1;
-                    System.out.printf("Number %d : %d is ODD", i , number);
-                }
-                else {
-                    number = number / 2;
-                    System.out.printf("Number %d : %d is EVEN", i, number);
-                }
-                System.out.println();
-                TimeUnit.MILLISECONDS.sleep(500);
+            else {
+                number = number / 2;
+                System.out.printf("Number %d : %d is EVEN\n", i, number);
             }
-        } catch (NumberFormatException ignored) {
-        }
+            if (withDelay) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets the number.
+     * @param number the number to be set.
+     */
+    public Collatz setNumber(Number number) {
+        if (!Validator.isWholeNumber().apply(String.valueOf(number))) throw new IllegalArgumentException(String.format("Invalid number: %f", number.doubleValue()));
+        if (Validator.isDecimalNumber().apply(String.valueOf(number))) throw new IllegalArgumentException("Only whole numbers are accepted");
+        Collatz.number = (int) number;
+        return this;
     }
 }
