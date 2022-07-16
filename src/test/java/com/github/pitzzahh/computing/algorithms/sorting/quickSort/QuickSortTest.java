5737dcf9efdcc297a6800940cfa3dff3531cb658
pitzzahh <araopj2191@gmail.com>
2022-07-16 14:25:01 +0800

    added test for quicksort

---
+++ src/test/java/com/github/pitzzahh/computing/algorithms/sorting/quickSort/QuickSortTest.java
@@ -0,0 +1,37 @@
+package com.github.pitzzahh.computing.algorithms.sorting.quickSort;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class QuickSortTest {
+
+    @Test
+    void shouldSortAByteArray() {
+        // given
+        var array = new Byte[] { 3, 1, 2 };
+        // when
+        QuickSort.sort(array);
+        // then
+        System.out.println(Arrays.toString(array));
+    }
+
+    @Test
+    void shouldSortAIntegerArray() {
+        // given
+        var array = new Integer[] { 3, 1, 2 };
+        // when
+        QuickSort.sort(array);
+        // then
+        System.out.println(Arrays.toString(array));
+    }
+
+    @Test
+    void shouldSortADoubleArray() {
+        // given
+        var array = new Double[] { 3.5, 1.5, 2.5 };
+        // when
+        QuickSort.sort(array);
+        // then
+        System.out.println(Arrays.toString(array));
+    }
+}
