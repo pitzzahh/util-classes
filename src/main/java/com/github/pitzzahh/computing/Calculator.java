7e14cc365da1637ec9b8f2083aff1f4b25af7f9c
peter <araopj2191@gmail.com>
2022-07-20 13:27:43 +0800

    integrated QuadrantAnalyzer class and HypotenuseCalculator class to Calculator class.

--- src/main/java/com/github/pitzzahh/computing/Calculator.java
+++ src/main/java/com/github/pitzzahh/computing/Calculator.java
@@ -1,48 +1,74 @@
 package com.github.pitzzahh.computing;
 
+
+import java.util.Arrays;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import com.github.pitzzahh.utilities.classes.enums.Quadrant;
+import static com.github.pitzzahh.utilities.classes.enums.Quadrant.*;
 
 /**
  * Utility Calculator class used for basic arithmetic operations.
  * @see Operation list of available arithmetic operation.
  * @param <T> the first number(any type of number)
- * @param <S> the second number(any type of number)
+ * @param <R> the second number(any type of number)
  */
-public final class Calculator <T extends Number, S extends Number> implements Operation {
+public final class Calculator <T extends Number, R extends Number, S extends Number> implements Operation {
 
-    private T firstNumber;
-    private S secondNumber;
+    private T a;
+    private R b;
+    private S c;
+    private BigDecimal result = BigDecimal.ZERO;
 
-    /**
-     * All args constructor for {@code Calculator} class.
-     * @param firstNumber the first number.
-     * @param secondNumber the second number.
-     */
-    public Calculator(T firstNumber, S secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-    }
-
+    private boolean isQuadrant;
     /**
      * No args Constructor.
      */
-    public Calculator() {
+    public Calculator() {}
+
+    /**
+     * Constructor for {@code Calculator} class. Taking only two valus. {@code T} and {@code R}
+     * @param a the first number.
+     * @param b the second number.
+     */
+    public Calculator(T a, R b) {
+        this.a = a;
+        this.b = b;
     }
 
     /**
-     * The method to be invoked in order to get the result.
-     * @param op the operation to be used.
-     * @return the result of the operation.
+     * All args constrcutor for {@code Calculator} class.
+     * @param a the first number.
+     * @param b the second number.
+     * @param c the third number.
+     */
+    public Calculator(T a, R b, S c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Abstract method used for calculation.
+     * @param operation the type of operation to be used.
+     * @return the calculated number based from the operations.
      * @throws IllegalArgumentException if the operation is invalid.
-     * @throws NullPointerException if any of the number is {@code null}.
      */
     @Override
-    public Number calculate(int op) throws IllegalArgumentException, NullPointerException {
-        if (firstNumber == null) throw new NullPointerException("Cannot invoke \"calculate(int)\" because first number is null");
-        else if (secondNumber == null) throw new NullPointerException("Cannot invoke \"calculate(int)\" because second number is null");
-        return getExact(op);
+    public Calculator calculate(int operation) throws IllegalArgumentException {
+        if (a == null && b == null) return new Calculator<>();
+        else if (a == null) throw new NullPointerException(String.format("Cannot invoke \"calculate(%s)\" because first number is null", operationUsed(operation)));
+        else if (b == null) throw new NullPointerException(String.format("Cannot invoke \"calculate(%s)\" because second number is null", operationUsed(operation)));
+        return getExact(operation);
+    }
+
+    /**
+     * Gets the result of the calculation/s.
+     * @return {@code Number} the calculated number.
+     */
+    public Number getCalculation() {
+        return this.result;
     }
 
     /**
@@ -52,26 +78,120 @@ public final class Calculator <T extends Number, S extends Number> implements Op
      * @return a {@code BigDecimal}, the calculated result.
      * @throws IllegalArgumentException if the operation is invalid.
      */
-    private Number getExact(int op) throws IllegalArgumentException {
-        if (op < MULTIPLY || op > MODULO) {
-            throw new IllegalArgumentException("Invalid Operation type");
+    private Calculator getExact(int op) throws IllegalArgumentException {
+        if (op < MULTIPLY || op > ANALYZE_QUADRANT) throw new IllegalArgumentException("Invalid Operation type " + op);
+        var a = new BigDecimal(this.a.toString());
+        var b = new BigDecimal(this.b.toString());
+        switch (op) {
+            case MULTIPLY -> this.result = this.result.add(a.multiply(b));
+            case DIVIDE -> this.result = this.result.add(a.divide(b));
+            case ADD -> this.result = this.result.add(a.add(b));
+            case SUBTRACT -> this.result = this.result.add(a.subtract(b));
+            case HYPOTENUSE_CALCULATION -> this.result = this.result.add(getHypotenuse());
+            case ANALYZE_QUADRANT -> {
+                isQuadrant = true;
+                result = BigDecimal.valueOf(whichQuadrant(this.a, this.b).ordinal());
+            }
         }
-        var a = new BigDecimal(this.firstNumber.toString());
-        var b = new BigDecimal(this.secondNumber.toString());
-        return op == MULTIPLY ? a.multiply(b) :
-                op == DIVIDE ? a.divide(b, new MathContext(RoundingMode.values().length)) :
-                        op == ADD ? a.add(b):
-                                op == SUBTRACT ? a.subtract(b) :
-                                        a.remainder(b);
+        return this;
     }
 
     /**
-     * Method that sets the numbers to be calculated.
+     * Computes the hypotenuse.
+     * @return the computed hypotenuse.
+     */
+    private BigDecimal getHypotenuse() {
+        var xValue = this.a instanceof Integer ? String.valueOf(this.a.intValue()) :
+                this.a instanceof Double ? String.valueOf(this.a.doubleValue()) :
+                        String.valueOf(this.a.byteValue());
+
+        var yValue = this.b instanceof Integer ? String.valueOf(this.b.intValue()) :
+                this.b instanceof Double ? String.valueOf(this.b.doubleValue()) :
+                        String.valueOf(this.b.byteValue());
+
+        var x = new BigDecimal(xValue);
+        var y = new BigDecimal(yValue);
+        return BigDecimal.valueOf(Math.sqrt((x.doubleValue() * x.doubleValue()) + (y.doubleValue() * y.doubleValue())));
+    }
+
+    /**
+     * Method that identifies which quadrant the point is.
+     * @param x the x position on the board
+     * @param y the y position on the board
+     * @return {@code Quadrant}
+     */
+    private static Quadrant whichQuadrant(Number x, Number y) {
+        if (x.intValue() > 0 && y.intValue()  > 0) return FIRST_QUADRANT;
+        else if (x.intValue()  < 0 && y.intValue()  > 0) return SECOND_QUADRANT;
+        else if (x.intValue()  < 0 && y.intValue()  < 0) return THIRD_QUADRANT;
+        else if (x.intValue()  > 0 && y.intValue()  < 0) return FOURTH_QUADRANT;
+        else return ORIGIN;
+    }
+
+    /**
+     * Gets the name of the operation that is used.
+     * @param operation the operation used.
+     * @return the {@code String} representation of the operation, if operation is invalid
+     */
+    private String operationUsed(int operation) {
+        return switch (operation) {
+            case 1 -> "MULTIPLY";
+            case 2 -> "DIVIDE";
+            case 3 -> "ADD";
+            case 4 -> "SUBTRACT";
+            case 5 -> "MODULO";
+            case 6 -> "HYPOTENUSE_CALCULATION";
+            default -> throw new IllegalArgumentException("Invalid Operation type " + operation);
+        };
+    }
+
+    /**
+     * Method that sets the first and second numbers to be calculated.
      * @param firstNumber the first number.
      * @param secondNumber the second number.
      */
-    public void setNumbers(T firstNumber, S secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
+    public void setNumbers(T firstNumber, R secondNumber) {
+        this.a = firstNumber;
+        this.b = secondNumber;
+    }
+
+    /**
+     * Method that sets all numbers to be calculated.
+     * @param firstNumber the first number.
+     * @param secondNumber the second number.
+     */
+    public void setNumbers(T firstNumber, R secondNumber, S thirdNumber) {
+        this.a = firstNumber;
+        this.b = secondNumber;
+        this.c = thirdNumber;
+    }
+
+    /**
+     * Gets the first parameter value.
+     * @return {@code T}, the first parameter value.
+     */
+    public T getA() {
+        return this.a;
+    }
+
+    /**
+     * Gets the second parameter value.
+     * @return {@code R}, the second parameter value.
+     */
+    public R getB() {
+        return this.b;
+    }
+
+    /**
+     * Gets the third parameter value.
+     * @return {@code C}, the third parameter value.
+     */
+    public S getC() {
+        return this.c;
+    }
+
+    @Override
+    public String toString() {
+        return isQuadrant ? Quadrant.valueOf(Arrays.stream(values()).filter(o -> o.ordinal() == result.intValue()).findAny().get().toString()).toString() : "";
     }
 }
