95e5732379e3671dacc54378b5601195140acb13
pitzzahh <araopj2191@gmail.com>
2022-07-23 23:35:00 +0800

    re-ordered imports. added getFileContents() method in FileUtil class

--- src/main/java/com/github/pitzzahh/utilities/FileUtil.java
+++ src/main/java/com/github/pitzzahh/utilities/FileUtil.java
@@ -1,10 +1,10 @@
 package com.github.pitzzahh.utilities;
 
-import java.io.File;
+import java.io.*;
+import java.util.List;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /**
  * Class used to some file operations.
@@ -15,20 +15,23 @@ public final class FileUtil {
      * Cannot instantiate this class
      */
     private FileUtil() {}
+
     /**
-     * Writes to a text file
-     * @param whatToWrite the {@code String} to be written on the file
+     * Writes to a text file.
+     * Text file will not be overriden when new contents is written, new contents will be written below the current content.
+     * @param whatToWrite the {@code String} to be written on the file.
      * @param fileToWrite the file to be written.
      * @throws IOException if the file does not exist.
      */
     public static void writeToATextFile(String whatToWrite, File fileToWrite) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite, true));
         bufferedWriter.write(whatToWrite);
+        bufferedWriter.newLine();
         bufferedWriter.close();
     }
 
     /**
-     *
+     * Read the contents of a file, each line.
      * @param fileToRead the file to be read
      * @throws IOException if the file does not exist.
      */
@@ -44,5 +47,17 @@ public final class FileUtil {
         else {
             System.out.println("SOMETHING WENT WRONG");
         }
+    }
+
+    /**
+     * Reads a file and adds it to a {@code List<String>}.
+     * A delimeter can be used.
+     * @param file the file to read.
+     * @param delimeter the delimeter
+     * @return a {@code List<String>} containing the contents of the file.
+     * @throws FileNotFoundException if the file does not exist.
+     */
+    public static List<String> getFileContents(File file, String delimeter) throws FileNotFoundException {
+        return new Scanner(file).useDelimiter(delimeter).tokens().toList();
     }
 }
