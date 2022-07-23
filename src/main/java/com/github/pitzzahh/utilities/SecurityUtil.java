500d7480824aa4547fbd26e60ec5a85af343f063
pitzzahh <araopj2191@gmail.com>
2022-07-23 23:22:23 +0800

    fixed issue: [#2] refactored encrypt() and decrypt() methods

--- src/main/java/com/github/pitzzahh/utilities/SecurityUtil.java
+++ src/main/java/com/github/pitzzahh/utilities/SecurityUtil.java
@@ -6,280 +6,53 @@ import javax.crypto.*;
 import java.security.*;
 import java.nio.charset.StandardCharsets;
 import javax.crypto.spec.GCMParameterSpec;
+import com.github.pitzzahh.computing.Operation;
+import com.github.pitzzahh.computing.Calculator;
+import static com.github.pitzzahh.utilities.classes.TextColors.*;
 
 /**
  * <p>The {@code SecurityUtil} class contains methods that encrypts and decrypts
  * in values of text files.</p>
  */
 public final class SecurityUtil {
-
-    private static final byte KEY = ((((10 * 3) + 4 ) / 4) * 2) - 26;
-    private static List<Character> EU;
-    private static List<Character> EP;
-    private static Scanner uScanner;
-    private static Scanner pScanner;
-    private static String u;
-    private static String p;
-    private static char[] uChar;
-    private static char[] pChar;
+    private static final byte KEY = 5;
 
     /**
      * Cannot instantiate this class
      */
-    private SecurityUtil() {}
-
-    /**
-     * Encrypts the user's username.
-     * @param usernameFile the file of where the username contained.
-     * @param user the username to encrypt.
-     * @throws IOException if the file does not exist.
-     */
-    public static void encryptUserName(File usernameFile, String user) throws IOException {
-        if (usernameFile.isFile()) {
-            u = "";
-            uScanner = new Scanner(usernameFile);
-            u = uScanner.nextLine();
-            uChar = u.toCharArray();
-            EU = new ArrayList<>(uChar.length);
-            for (char element : uChar) {
-                element += (KEY * (-10));
-                EU.add(element);
-            }
-            u = Utility.convertToString(EU);
-            FileUtil.writeToATextFile(u, usernameFile);
-            writeUserNameToBin(usernameFile, user);
-            EU.clear();
-            uScanner.close();
-            resetUChar(uChar);
-        }
-        else {
-            System.out.println("SOMETHING WENT WRONG");
-        }
-    }
-    /**
-     * Encrypts the user's pin.
-     * @param pinFile the file where the pin contained.
-     * @param pin the pin to encrypt.
-     * @throws IOException if the file does not exist.
-     */
-    public static void encryptPin(File pinFile, String pin) throws IOException {
-        if (pinFile.isFile()) {
-            p = "";
-            pScanner = new Scanner(pinFile);
-            p = pScanner.nextLine();
-            pChar = p.toCharArray();
-            EP = new ArrayList<>(pChar.length);
-            for (char element : pChar) {
-                element += (KEY * (-10) + 4);
-                EP.add(element);
-            }
-            p = Utility.convertToString(EP);
-            FileUtil.writeToATextFile(p, pinFile);
-            writePinToBin(pinFile, pin);
-            EP.clear();
-            pScanner.close();
-            resetPChar(pChar);
-        }
-        else {
-            System.out.println("SOMETHING WENT WRONG");
-        }
+    private SecurityUtil() {
     }
 
     /**
-     * Decrypts the username from a text file.
-     * @param encryptedUsername the encrypted username file.
+     * Encrypts a text.
+     * @param text the text to encrypt.
+     * @throws IllegalArgumentException if a text is empty.
+     */
+    public static String encrypt(String text) throws IllegalArgumentException {
+        if (text.isEmpty()) throw new IllegalArgumentException("Text to be encrypted cannot be empty");
+        var array = text.toCharArray();
+        var list = new ArrayList<>(array.length);
+        for (char bit : array) {
+            bit += (KEY + (-10) + 4);
+            list.add(bit);
+        }
+        return Utility.convertToString(list);
+    }
+
+    /**
+     * Decrypts a text.
+     * @param text the encrypted username file.
      * @return the decrypted username.
      */
-    private static String decryptUserName(String encryptedUsername) {
-        uChar = encryptedUsername.toCharArray();
-        EU.clear();
-        EU = new ArrayList<>(uChar.length);
-        for (char bit : uChar) {
-            bit -= (KEY * (-10));
-            EU.add(bit);
+    public static String decrypt(String text) {
+        if (text.isEmpty()) throw new IllegalArgumentException("Text to be decrypted cannot be empty");
+        var array = text.toCharArray();
+        var list = new ArrayList<>(array.length);
+        for (char bit : array) {
+            bit -= (KEY + (-10) + 4);
+            list.add(bit);
         }
-        resetUChar(uChar);
-        return u = Utility.convertToString(EU);
-    }
-
-    /**
-     * Decrypts the pin from a text file.
-     * @param encryptedPin the encrypted pin file.
-     * @return the decrypted pin.
-     */
-    private static String decryptPin(String encryptedPin) {
-        pChar = encryptedPin.toCharArray();
-        EP.clear();
-        EP = new ArrayList<>(pChar.length);
-        for (char bit : pChar) {
-            bit -= (KEY * (-10) + 4);
-            EP.add(bit);
-        }
-        resetPChar(pChar);
-        return p = Utility.convertToString(EP);
-    }
-
-    /**
-     * Checks if the username and password matched the user's credentials.
-     * @param usernameFile the file of where the username contained.
-     * @param pinFile the file where the password contained.
-     * @param username the username to check
-     * @param pin the pin to check.
-     * @param isAdmin if is an admin or not, admins are only capable of viewing the credentials.
-     * @return {@code true} if username and pin matches the username and pin arguments.
-     * @throws IOException if the file does not exist.
-     */
-    public static boolean checkCredentials(File usernameFile, File pinFile, String username, String pin, boolean isAdmin) throws IOException {
-        try {
-            uScanner = new Scanner(usernameFile);
-            pScanner = new Scanner(pinFile);
-            u = uScanner.nextLine();
-            p = pScanner.nextLine();
-            uChar = u.toCharArray();
-            pChar = p.toCharArray();
-            EU = new ArrayList<>(uChar.length);
-            EP = new ArrayList<>(pChar.length);
-            for (char bit : uChar) {
-                bit -= (KEY * (-10));
-                EU.add(bit);
-            }
-            for (char bit : pChar) {
-                bit -= (KEY * (-10) + 4);
-                EP.add(bit);
-            }
-            u = Utility.convertToString(EU);
-            p = Utility.convertToString(EP);
-            EU.clear();
-            EP.clear();
-            uScanner.close();
-            pScanner.close();
-            resetUChar(uChar);
-            resetPChar(pChar);
-        } catch (FileNotFoundException ignored) {}
-        return username.equals(u) && pin.equals(p) && readFromBin(username, isAdmin, username, pin);
-    }
-
-    /**
-     * Method that reWrite the user's username to a binary file.
-     * @param usernameFile the file of where the username contained.
-     * @throws IOException if the file does not exist.
-     */
-    private static void writeUserNameToBin(File usernameFile, String user) throws IOException {
-        try {
-            uScanner = new Scanner(usernameFile);
-            u = uScanner.nextLine();
-            DataOutputStream dataOutputStreamForUserName = new DataOutputStream(new FileOutputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\uCache.bin"));
-            dataOutputStreamForUserName.writeUTF(u);
-            dataOutputStreamForUserName.flush();
-            dataOutputStreamForUserName.close();
-            uScanner.close();
-        } catch (FileNotFoundException ignored) {}
-    }
-
-    /**
-     * Method that reWrite the user's pin to a binary file.
-     * @param pinFile the file where the pin contained.
-     * @throws IOException if the file does not exist.
-     */
-    private static void writePinToBin(File pinFile, String user) throws IOException {
-        try {
-            p = "";
-            pScanner = new Scanner(pinFile);
-            p = pScanner.nextLine();
-            DataOutputStream dataOutputStreamForPin = new DataOutputStream(new FileOutputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\pCache.bin"));
-            dataOutputStreamForPin.writeUTF(p);
-            dataOutputStreamForPin.flush();
-            dataOutputStreamForPin.close();
-            pScanner.close();
-        } catch (FileNotFoundException ignored) {}
-    }
-
-    /**
-     * Method that rechecks the user's credentials.
-     * <p>Even though the user copied and pasted the username and password from the txt file, it still won't logged in
-     * unless the user really know his/her credentials.</p>
-     * @param user the username of the user.
-     * @param isAdmin checks if the person who wants to log in is as admin.
-     * @throws IOException if the file does not exist.
-     */
-    private static boolean readFromBin(String user, boolean isAdmin, String username, String pin) throws IOException {
-        try {
-            u = "";
-            p = "";
-            DataInputStream dataInputStreamForUserName;
-            DataInputStream dataInputStreamForPin;
-            if (isAdmin) {
-                dataInputStreamForUserName = new DataInputStream(new FileInputStream("C:\\Users\\Public\\Cache\\auth\\uCache.bin"));
-                dataInputStreamForPin = new DataInputStream(new FileInputStream("C:\\Users\\Public\\Cache\\auth\\pCache.bin"));
-            }
-            else {
-                dataInputStreamForUserName = new DataInputStream(new FileInputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\uCache.bin"));
-                dataInputStreamForPin = new DataInputStream(new FileInputStream("C:\\Users\\Public\\Cache\\" + user + "'s Folder\\pCache.bin"));
-            }
-            String uCache = dataInputStreamForUserName.readUTF();
-            String pCache = dataInputStreamForPin.readUTF();
-            u = decryptUserName(uCache);
-            p = decryptPin(pCache);
-            dataInputStreamForUserName.close();
-            dataInputStreamForPin.close();
-        } catch (FileNotFoundException ignored) {
-        }
-        return u.equals(username) && p.equals(pin);
-    }
-
-    /**
-     * Method that used by an admin to view users credentials by decrypting all users credentials.
-     * @param userName the username file to be decrypted.
-     * @param pin the pin file to be decrypted.
-     * @param isAdmin checks if the one that use this is an admin
-     * @return a {@code String} that contains the user's username and pin.
-     * @throws FileNotFoundException if the credentials file does not exist
-     */
-    public static String viewCredentials(File userName, File pin, boolean isAdmin) throws FileNotFoundException {
-        if (isAdmin) {
-            u = "";
-            p = "";
-            uScanner = new Scanner(userName);
-            pScanner = new Scanner(pin);
-            u = uScanner.nextLine();
-            p = pScanner.nextLine();
-            uChar = u.toCharArray();
-            pChar = p.toCharArray();
-            EU = new ArrayList<>(uChar.length);
-            EP = new ArrayList<>(pChar.length);
-            for (char bit : uChar) {
-                bit -= (KEY * (-10));
-                EU.add(bit);
-            }
-            for (char bit : pChar) {
-                bit -= (KEY * (-10) + 4);
-                EP.add(bit);
-            }
-            u = Utility.convertToString(EU);
-            p = Utility.convertToString(EP);
-            EU.clear();
-            EP.clear();
-            uScanner.close();
-            pScanner.close();
-        }
-        else return "you'reNotAllowedToAccessThis  you're'NotAnAdministrator";
-        return u.concat(" " + p).strip();
-    }
-
-    /**
-     * Resets the username char array.
-     * @param uChar the username char to be refilled with empty space.
-     */
-    private static void resetUChar(char[] uChar) {
-        Arrays.fill(uChar, ' ');
-    }
-
-    /**
-     * Resets the pin char array.
-     * @param pChar the pin char to be refilled with empty space.
-     */
-    private static void resetPChar(char[] pChar) {
-        Arrays.fill(pChar, ' ');
+        return Utility.convertToString(list);
     }
 
     /**
