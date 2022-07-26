985b7a304e95a7fb3ca10dbb36b605c7084d7e01
pitzzahh <araopj2191@gmail.com>
2022-07-26 14:54:54 +0800

    fixed issue [#3]

--- src/main/java/com/github/pitzzahh/utilities/validation/Validator.java
+++ src/main/java/com/github/pitzzahh/utilities/validation/Validator.java
@@ -103,7 +103,7 @@ public interface Validator extends Predicate<String> {
      */
     // TODO: fix bug [year is valid when year is: [ 1 or 2]0[0 or 1 or 2]any single digit ]
     static Validator isBirthDateValid() {
-        return input -> Pattern.compile("^(1[8|9][5-9]\\d|[1-2]0[0-2]\\d)-([1-9]{1}|0[1-9]|1[0-2])-(\\d{1}|0[\\d]|[1-2]\\d|[1-3][0-1])$").matcher(input).matches();
+        return input -> Pattern.compile("^(1[8|9][5-9]\\d|20[0-2]\\d|19[\\d]\\d|)-([1-9]{1}|0[1-9]|1[0-2]|)-(\\d{1}|0[\\d]|[1-2]\\d|[1-3][0-1])$").matcher(input).matches();
     }
 
     /**
