e969148fa02ba56401f5a305105d2bf957b93f0e
pitzzahh <araopj2191@gmail.com>
2022-07-29 09:56:09 +0800

    added comments

--- src/test/java/com/github/pitzzahh/utilities/SecurityUtilTest.java
+++ src/test/java/com/github/pitzzahh/utilities/SecurityUtilTest.java
@@ -1,16 +1,18 @@
 package com.github.pitzzahh.utilities;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SecurityUtilTest {
 
     @Test
     void shouldDecrypt() {
+        // given
         var text = "200263444";
+        // when
         var encrypted = SecurityUtil.encrypt(text);
         var decryped = SecurityUtil.decrypt(encrypted);
+        // then
         assertEquals("200263444", decryped);
     }
 
