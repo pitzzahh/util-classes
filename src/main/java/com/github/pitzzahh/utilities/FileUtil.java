0edd071755feaaff5e416aea43ec3bfd41986b5c
pitzzahh <araopj2191@gmail.com>
2022-08-15 22:30:24 +0800

    refactored readFile() and getFileContents() method.

--- src/main/java/com/github/pitzzahh/utilities/FileUtil.java
+++ src/main/java/com/github/pitzzahh/utilities/FileUtil.java
@@ -2,7 +2,9 @@ package com.github.pitzzahh.utilities;
 
 import java.io.*;
 import java.util.List;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 /**
  * Class used to some file operations.
@@ -21,8 +23,8 @@ public final class FileUtil {
      * @param fileToWrite the file to be written.
      * @throws IOException if the file does not exist.
      */
-    public static void writeToATextFile(String whatToWrite, File fileToWrite) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite, true));
+    public static void writeToATextFile(String whatToWrite, File fileToWrite, boolean writeToBottom) throws IOException {
+        var bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite, writeToBottom));
         bufferedWriter.write(whatToWrite);
         bufferedWriter.newLine();
         bufferedWriter.close();
@@ -31,31 +33,26 @@ public final class FileUtil {
     /**
      * Read the contents of a file, each line.
      * @param fileToRead the file to be read
-     * @throws IOException if the file does not exist.
+     * @throws IOException if the file is not a valid file or it does not exist.
      */
-    public static void readFile(File fileToRead) throws IOException {
-        if (fileToRead.exists() && fileToRead.isFile()) {
-            Scanner fileScanner = new Scanner(fileToRead);
-            while (fileScanner.hasNextLine()) {
-                String data = fileScanner.nextLine();
-                System.out.println(data);
-            }
-            fileScanner.close();
-        }
-        else {
-            System.out.println("SOMETHING WENT WRONG");
-        }
+    public static void printFileContents(File fileToRead) throws IOException {
+        if (!fileToRead.exists() || !fileToRead.isFile()) throw new IllegalStateException("INVALID FILE");
+        Files.lines(Paths.get(fileToRead.getAbsolutePath()))
+                .forEach(Print::println);
     }
 
     /**
-     * Reads a file and adds it to a {@code List<String>}.
-     * A delimeter can be used.
-     * @param file the file to read.
-     * @param delimeter the delimeter
-     * @return a {@code List<String>} containing the contents of the file.
-     * @throws FileNotFoundException if the file does not exist.
+     * Gets the file contents of each line, splits them using a delimiter and storing each line to an array.
+     * @param file the file to get the contents
+     * @param line what line in the text file to skip, if 0, no lines will be skipped
+     * @param delimeter the line seperator
+     * @return a {@code List<String[]>} the contents of the file.
+     * @throws IOException if something is wrong with file operations.
      */
-    public static List<String> getFileContents(File file, String delimeter) throws FileNotFoundException {
-        return new Scanner(file).useDelimiter(delimeter).tokens().toList();
+    public static List<String[]> getFileContents(File file, int line, String delimeter) throws IOException {
+        return Files.lines(Paths.get(file.getAbsolutePath()))
+                .skip(line)
+                .map(s -> s.contains("") ? s.split("") : s.split(delimeter))
+                .collect(Collectors.toList());
     }
 }
