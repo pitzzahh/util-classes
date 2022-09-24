74cca51d567679b6b46df447a3a2f71d74d93bf2
pitzzahh <araopj2191@gmail.com>
2022-09-24 12:18:27 +0800

    fixed bug where files are not seperated by a separator

--- src/main/java/io/github/pitzzahh/utilities/FileUtil.java
+++ src/main/java/io/github/pitzzahh/utilities/FileUtil.java
@@ -1,11 +1,14 @@
 package io.github.pitzzahh.utilities;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Class used to some file operations.
@@ -37,6 +40,7 @@ public final class FileUtil {
      * @param fileToRead the file to be read
      * @throws IOException if the file is not a valid file, or it does not exist.
      */
+    @SuppressWarnings("resource")
     public static void printFileContents(File fileToRead) throws IOException {
         Objects.requireNonNull(fileToRead, "File cannot be null");
         Files.lines(Paths.get(fileToRead.getAbsolutePath()))
@@ -47,15 +51,19 @@ public final class FileUtil {
      * Gets the file contents of each line, splits them using a delimiter and storing each line to an array.
      * @param file the file to get the contents
      * @param line what lines in the text file to skip, if 0, no lines will be skipped
-     * @param delimiter the line separator
+     * @param separator the line separator, if null, wil throw NullPointerException. To separate
+     *                  every character in each line use an empty {@code String} ""
      * @return a {@code List<String[]>} the contents of the file.
      * @throws IOException if something is wrong with file operations.
      */
-    public static List<String[]> getFileContents(File file, int line, String delimiter) throws IOException {
+    @SuppressWarnings("resource")
+    public static List<String[]> getFileContents(File file, int line, String separator) throws IOException {
         Objects.requireNonNull(file, "File cannot be null");
+        Objects.requireNonNull(separator, "separator cannot be null, please use an empty String to separate everything (\"\")");
         return Files.lines(Paths.get(file.getAbsolutePath()))
                 .skip(line)
-                .map(s -> s.split(delimiter))
+                .map(a -> Arrays.stream(a.split(separator)).map(String::trim).collect(Collectors.joining(separator)))
+                .map(e -> e.split(separator))
                 .collect(Collectors.toList());
     }
 }
